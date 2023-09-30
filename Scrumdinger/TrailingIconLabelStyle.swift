ec21ff990a2337f894e5b7a7cfcf03a01b5fd276
abdulkerim.karaman <abdulkerim.karaman@albarakatech.com>
2023-09-30 13:35:14 +0300

    first commit

---
+++ Scrumdinger/TrailingIconLabelStyle.swift
@@ -0,0 +1,23 @@
+//
+//  TrailingIconLabelStyle.swift
+//  Scrumdinger
+//
+//  Created by Abdulkerim Karaman on 30.09.2023.
+//
+
+import SwiftUI
+
+
+struct TrailingIconLabelStyle: LabelStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        HStack {
+            configuration.title
+            configuration.icon
+        }
+    }
+}
+
+
+extension LabelStyle where Self == TrailingIconLabelStyle {
+    static var trailingIcon: Self { Self() }
+}
